536434cffea63e478ad50e0a33dddd6993b660d8
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-04-20 13:04:06 +0300

    Updates

---
+++ precache-manifest.f3844c79769c9737028f262ebccbbb17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda6982bdadf1c95236fc4c70d4f4448",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "30ea286dc7e62dfad35f",
+    "url": "/goit-react-hw-02-feedback/static/js/2.51c57ae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.51c57ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef5642dc7c15db036e4",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f9d380ea.chunk.js"
+  },
+  {
+    "revision": "1a2af30d5f9b5771679f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.a031b346.js"
+  }
+]);

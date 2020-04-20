a3ccbf00013f8ec9a5f39f5c1268c9cf335e395d
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-04-20 21:26:05 +0300

    some styles added

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./FeedbackOptions.module.css";
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
       {Object.keys(options).map((key) => (
-        <button type="button" name={key} key={key} onClick={onLeaveFeedback}>
+        <button
+          type="button"
+          name={key}
+          key={key}
+          onClick={onLeaveFeedback}
+          className={styles.optionBtn}
+        >
           {key}
         </button>
       ))}
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./Statistics.module.css";
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
-    <ul>
-      <li>Good: {good}</li>
-      <li>Neutral: {neutral}</li>
-      <li>Bad: {bad}</li>
-      <li>Total: {total}</li>
-      <li>Positive feedback: {positivePercentage}%</li>
+    <ul className={styles.statisticsList}>
+      <li className={styles.statisticsListItem}>
+        <b>Good:</b> {good}
+      </li>
+      <li className={styles.statisticsListItem}>
+        <b>Neutral:</b> {neutral}
+      </li>
+      <li className={styles.statisticsListItem}>
+        <b>Bad:</b> {bad}
+      </li>
+      <li className={styles.statisticsListItem}>
+        <b>Total:</b> {total}
+      </li>
+      <li className={styles.statisticsListItem}>
+        <b> Positive feedback: </b>
+        {positivePercentage}%
+      </li>
     </ul>
   );
 };

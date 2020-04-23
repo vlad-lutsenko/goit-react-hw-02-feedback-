c8a88216344daf1d0dd2b6b8645ca3a19a16c68a
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-04-23 16:30:32 +0300

    fixed

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,20 +5,12 @@ import styles from "./Statistics.module.css";
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <ul className={styles.statisticsList}>
-      <li className={styles.statisticsListItem}>
-        <b>Good:</b> {good}
-      </li>
-      <li className={styles.statisticsListItem}>
-        <b>Neutral:</b> {neutral}
-      </li>
-      <li className={styles.statisticsListItem}>
-        <b>Bad:</b> {bad}
-      </li>
-      <li className={styles.statisticsListItem}>
-        <b>Total:</b> {total}
-      </li>
-      <li className={styles.statisticsListItem}>
-        <b> Positive feedback: </b>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad:{bad}</li>
+      <li>Total: {total}</li>
+      <li>
+        Positive feedback:
         {positivePercentage}%
       </li>
     </ul>
